14ef6c7a9f6c7de475f69f284d3e0a2b7b3d731d
Victor <victor.kolev@gmail.com>
2026-04-14 02:30:25 +0300

    da2

--- desk/src/main/java/com/edutech/desk/serviceimpl/UserServiceImpl.java
+++ desk/src/main/java/com/edutech/desk/serviceimpl/UserServiceImpl.java
@@ -3,7 +3,6 @@ package com.edutech.desk.serviceimpl;
 import com.edutech.desk.entities.User;
 import com.edutech.desk.repository.UserRepository;
 import com.edutech.desk.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import java.util.List;
@@ -11,11 +10,13 @@ import java.util.List;
 @Service
 public class UserServiceImpl implements UserService {
 
-    @Autowired
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    @Autowired
-    private PasswordEncoder passwordEncoder;
+    public UserServiceImpl(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
 
     @Override
     public User login(String email, String password) {
